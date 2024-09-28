bd6f8086c992c5a6aa7ce92335f53db285101c77
Ester Luiza <luiza.campossouza.25@gmail.com>
2024-09-28 19:53:14 -0300

    projeto criado

---
+++ script.js
@@ -0,0 +1,49 @@
+// Desafio primeiro projeto Felipao DIO
+// Ester Luiza 
+
+// Variavéis
+   
+   
+// Matriz contendo o nome do personagem e o XP 
+let personagens = [
+    ["Blank", 1000],
+    ["Whity", 2000],
+    ["Penki", 7000],
+    ["Branw", 8000],
+    ["Purpu", 9000],
+    ["Grii", 10000],
+    ["Gray", 11000]
+];
+
+let i = 0;
+do {
+    // Variáveis
+    let nome = personagens[i][0]; // Nome do personagem (primeira coluna da matriz)
+    let xp = personagens[i][1];   // XP do personagem (segunda coluna da matriz)
+    let nivel;
+
+    // Estrutura de decisão com if e else if
+    if (xp < 1001) {
+        nivel = "Ferro";
+    } else if (xp >= 1000 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 10000) {
+        nivel = "Radiante";
+ 
+    }
+
+    // Saída
+    console.log(`O personagem de nome ${nome} está no nível ${nivel}`);
+
+    i++; // Incrementa o índice para o próximo personagem
+} while (i < personagens.length);
+
+
